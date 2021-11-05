3359df13e4bf8729e8e56c411ee371a28c89bf89
shamss17 <shamstarek13@gmail.com>
2021-11-05 22:08:08 +0200

    after fixing addflight

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Flightss from './components/Flightss';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <addFlight />
+    <Flightss/>
   </React.StrictMode>,
   document.getElementById('root')
 );
