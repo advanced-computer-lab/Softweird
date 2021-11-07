53f5c41c0ec5bbf48f9f1666acda7cd76e20156f
shamss17 <shamstarek13@gmail.com>
2021-11-07 13:12:46 +0200

    shakl

--- my-app/src/components/Flightss.js
+++ my-app/src/components/Flightss.js
@@ -4,10 +4,13 @@ import TextField from '@mui/material/TextField';
 
 export default function TextFieldSizes() {
   return (
+
+
     <Box
+
       component="form"
       sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
+        '& .MuiTextField-root': { m: 2, width: '25ch' },
       }}
       noValidate
       autoComplete="off"
